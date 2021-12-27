da55e33f4519c33244b8afcfcbc7d8393413676d
Alisjj <aliyussajjads@gmail.com>
2021-12-28 00:19:09 +0100

    email

--- CRM/settings.py
+++ CRM/settings.py
@@ -166,7 +166,7 @@ if not DEBUG:
 
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = env("EMAIL_HOST")
-    EMAIL_HOST_USER = env("EMAIL_HEMAIL_HOST_USEROST")
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
     EMAIL_USE_TLS = env("EMAIL_USE_TLS")
     EMAIL_PORT = env("EMAIL_PORT")
